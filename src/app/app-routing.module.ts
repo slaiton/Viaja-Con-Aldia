c2dfeb74f3ff7461fcb21ba3de17dae8bdb1e80d
Stiven Laiton <69602896+slaiton@users.noreply.github.com>
2025-10-06 11:02:39 -0500

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
   },
   {
     path: 'registro',
-    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule),
+    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
   },
   {
     path: 'turnos',
